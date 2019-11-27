d79514bbf53abccc82020edd1a95c140928e0fd2
Andres Felipe Lopez <UTP@Mac31.local>
2019-11-27 08:42:26 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule, StorageBucket } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatMenuModule} from '@angular/material/menu';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'my-app-name'), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, BrowserAnimationsModule, // imports firebase/storage only needed for storage features
+    MatMenuModule,
   ],
-  providers: [],
+  providers: [
+    AngularFireAuthGuard,
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAxaWmFO1W0pPIuaTK6mnZTLZKEttFVN2Y",
+    authDomain: "ant-app-7ee04.firebaseapp.com",
+    databaseURL: "https://ant-app-7ee04.firebaseio.com",
+    projectId: "ant-app-7ee04",
+    storageBucket: "ant-app-7ee04.appspot.com",
+    messagingSenderId: "593354332989",
+    appId: "1:593354332989:web:7d7408657cce6b84"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
